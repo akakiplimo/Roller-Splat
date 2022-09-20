5e2f7b9cf60788591676fc3696849ef8f0152df2
A. Kiplimo <akakiplimo@gmail.com>
2022-09-20 22:24:33 +0300

    Initial commit

---
+++ Assets/Scripts/GroundPiece.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPiece : MonoBehaviour
+{
+    // declare variables
+    public bool isColored = false;
+
+    public void ChangeColor(Color color)
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Roller-Splat
+ An Android based Game Developed using Unity C# which implements a hyper-casual game specifically built for mobile 
