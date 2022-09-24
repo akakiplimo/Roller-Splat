30776fffbee126662ff47950932673ea9b432f18
A. Kiplimo <akakiplimo@gmail.com>
2022-09-24 13:19:57 +0300

    5 Levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
 
     private void NextLevel()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 1)
+        if (SceneManager.GetActiveScene().buildIndex == 4)
         {
             SceneManager.LoadScene(0);
         }
